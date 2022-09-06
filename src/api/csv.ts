3bf1575cb549e50fd484133e2d60fe6d82a3523b
Ryan Gonzales <ryan_gonzales@spe.sony.com>
2022-09-06 15:47:06 -0700

    Add dotenv, axios and call to the lambda function

--- src/api/csv.ts
+++ src/api/csv.ts
@@ -1,8 +1,15 @@
 import { Command } from 'commander'
 import fs from 'fs'
 import { parse } from 'csv-parse'
+import axios from 'axios'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const program = new Command()
+
+const INTERVAL_MS = 5000
+
+const { TOKEN, URL } = process.env
 
 program
   .option('-f, --file <file...>', 'file location of csv')
@@ -14,56 +21,71 @@ program
       console.log('must provide file location')
       process.exit(1)
     }
-    
-    if (divideByCount?.[0]) {
-      console.log(parseInt(divideByCount[0]))
-      if (!Number.isInteger(parseInt(divideByCount[0]))) {
-        
-        console.log('must provide a valid number')
-        process.exit(1)
-      }
+
+    if (!divideByCount?.[0]) {
+      console.log('must provide a valid number to divide rows into')
+      process.exit(1)
     }
   })
   .parse()
 
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms))
+
 export const runCsv = () => {
   const { file, divideByCount } = program.opts()
-  
+
   const path = file[0]
   const count = divideByCount?.[0]
 
-  const results: any[] = [];
-  
-  const dividedResults: string[][] = []
+  const results: any[] = []
 
   fs.createReadStream(path)
     .pipe(parse())
     .on('data', (data: string[]) => results.push(data))
-    .on('end', () => {
+    .on('end', async () => {
       const [header, ...rows] = results
-      
+
       const title = header[0]
-      console.log('title', title)
-      
+      console.log('title: ', title)
+
       const allRows = rows.flat()
-      
+
+      console.log('rows count: ', allRows.length)
+
       if (count) {
         let currentResult: string[] = []
-        
-        allRows.forEach((rows,  index) => {
 
-          currentResult.push(rows)
-          
+        for (let index = 0; index < allRows.length; index++) {
+          currentResult.push(allRows[index])
+
           if ((index + 1) % parseInt(count) === 0) {
-            console.log('current result', currentResult)
-            dividedResults.push(currentResult)
+            console.log('Sending out current object ids', currentResult)
+            
+            try {
+              const response = await axios.post(URL as string, [...currentResult], {
+                headers: {
+                  Authorization: `Bearer ${TOKEN}`,
+                  'content-type': 'application/json',
+                },
+              })
+              
+              console.log('response', response)
+            } catch (error) {
+              console.error('error', error)
+              console.error('errored out at current result', currentResult)
+              process.exit(1)
+            }
+             
+            console.log('finished sending')
+            
+            await delay(INTERVAL_MS);
             currentResult = []
           }
-        })
+        }
       }
-      console.log(dividedResults)
-    });
-  
+
+    })
+
 }
 
 runCsv()
